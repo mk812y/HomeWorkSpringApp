46c397b986529bc8d3f86c08d47e5f6a11e5d02f
mk812y <mk812y@gmail.com>
2022-01-11 21:19:50 +0300

    Initial Commit

---
+++ HomeWorkSpringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkSpringApp
+//
+//  Created by Михаил Куприянов on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
